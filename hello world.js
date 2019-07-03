5f8c51330bdcb98bf6b072617186770f843fa2f1
Osa <39202977+osaimola@users.noreply.github.com>
2019-07-03 07:11:47 +0100

    Update hello world.js

--- hello world.js
+++ hello world.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 let gender;
+
 function pickAStruggle() {
-  if (Math.random() < .5){return 'girl'}
-                          else{return 'boy'};
+  if (Math.random() < .5) {
+	  return 'girl'
+  } else {
+	  return 'boy'
+  };
 }
 
 gender = pickAStruggle();
 //inject JS into JSX element with curly braces and use tenary op for conditional
 const body = (
 	<body>
-    <p>Well....</p>
-		{gender === 'girl' ? <p>Hello Miss</p> : <p>Hello Sir</p>} 
+	<p>Well....</p>
+	{gender === 'girl' ? <p>Hello Miss</p> : <p>Hello Sir</p>} 
 	</body>
 );
 
