9e166cf485e03af27225b03e9fca4fdd0c14eb78
Osa <39202977+osaimola@users.noreply.github.com>
2019-07-17 22:51:05 +0100

    Create component 3.js

---
+++ component 3.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const fiftyFifty = Math.random() < 0.5;
+
+// New component class starts here:
+class TonightsPlan extends React.Component {
+  // variables/constants can only go into methods we define in our class. errors are cause outside methods
+  render() {
+    let plan;
+    if (fiftyFifty) {
+      plan = <h1>Tonight I'm going out WOOO</h1>;
+    }  else {
+      plan = <h1>Tonight I'm going to bed WOOO</h1>;
+    }
+    
+    return plan;
+  }
+}
+
+ReactDOM.render(<TonightsPlan />,
+               document.getElementById('app'));
