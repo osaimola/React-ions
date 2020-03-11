a47ba8f841caafbbc2b93683c2e1bdc50c3f59a9
Osa <39202977+osaimola@users.noreply.github.com>
2020-03-10 22:55:27 -0600

    Update hooks.js

--- hooks.js
+++ hooks.js
@@ -3,16 +3,21 @@ import React, { useState, useEffect } from "react";
 function App() {
   let fruitDump = { type: "berry", name: "strawberry" };
   const [fruit, setFruit] = useState(fruitDump);
+  const [quality, setQuality] = useState("juicy");
+
   useEffect(() => {
     document.title = "Juicy " + fruit.name;
-    console.log("done");
+    console.log("this will run on every render");
     alert("reloaded");
 
     return () => alert("are you sure you want to close this window?");
   });
 
-  const [quality, setQuality] = useState("juicy");
-
+  useEffect(() => {
+    alert("Welcome to Juicy Fruity");
+    console.log("this will run only on the first render");
+  }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
